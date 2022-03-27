49dd39ab37eb86e76a7ae24dbe5675401db2485e
Jamond Turner <jamondturner@yahoo.com>
2022-03-27 15:44:19 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,35 +1,126 @@
 // Code your solution in this file!
-const hqLocation = 50
-let pickupLocation = 42
-let blocks = (hqLocation-pickupLocation)
-
-function distanceFromHqInBlocks(pickupLocation, hqLocation){
-return ((hqLocation-pickupLocation) `in ${blocks}`);
+// const hqLocation = 43
+// let pickupLocation = 42
+// let blocks = (hqLocation-pickupLocation)
 
 
+// // let number = 43
+// // let number2 = 42
+// const testOne = function distanceFromHqInBlocks(num1){
+// return num1 - num2
 
-}
+// }
 
+// // Test One (43,42)
 
+let xa = 43
+let ya = 42
 
+function distanceFromHqInBlocks(xa, ya) {
 
-
-function distanceFromHqInFeet(){
-
-}
-
-
-
-
-function distanceTravelledInFeet(){
+    return xa - ya
 
 }
+console.log(distanceFromHqInBlocks(43, 42));
+distanceFromHqInBlocks(43, 42);
+
+// Test Two (50,42)
+
+let xb = 50
+let yb = 42
+
+function distanceFromHqInBlocks(xb,yb){
+    return xb-yb
+}
+console.log(distanceFromHqInBlocks(50,42));
+distanceFromHqInBlocks(50,42)
+
+// Test Three (42,34)
+let xc = 42
+let yc = 34
+
+function distanceFromHqInBlocks(xc,yc){
+    return xc-yc
+}
+console.log(distanceFromHqInBlocks(42,34));
+distanceFromHqInBlocks(42,34);
+
+// Test 4
+// 1 Block equals 264 feet
+
+ let xd = 1  //block
+
+function distanceFromHqInFeet(xd) {
+    return xd * 264
+}
+console.log(distanceFromHqInFeet(xd))
+distanceFromHqInFeet(1);
+
+// Test 5
+
+let xe = 8  // Blocks
+
+function distanceFromHqInFeet(xe) {
+    return xe * 264
+}
+console.log(distanceFromHqInFeet(8));
+distanceFromHqInFeet(8);
+
+// Test 6
+
+let xf = 8 //blocks
+
+function distanceFromHqInFeet(xf) {
+    return xf * 264
+}
+console.log(distanceFromHqInFeet(8));
+distanceFromHqInFeet(8);
 
 
+// Test #7,8,9 
+// Block 48 to Block 43 = 5 Blocks Total =  1320 Feet
+// Block 60 to Block 50 = 10 Blocks Total = 2640 Feet
+// Block 34 to Block 28 = 6 Blocks Total =  1584 Feet
+// 1 Block = 264 Feet
 
-function calculatesFarePrice(start, destination){
+
+let za = 5
+
+function distanceTravelledInFeet(za) {
+    return za * 264
+}
+console.log(distanceTravelledInFeet(5));
+distanceTravelledInFeet(5);
+
+let zb = 10
+
+function distanceTravelledInFeet(zb) {
+    return zb * 264
+}
+console.log(distanceTravelledInFeet(10));
+distanceTravelledInFeet(10);
+
+let zc = 6
+
+function distanceTravelledInFeet(zc){
+    return zc * 264
+}
+console.log(distanceTravelledInFeet(6));
+distanceTravelledInFeet(6);
 
 
+// Test 10,11,12,13
+// 1320 Feet, 2640 Feet, 1584 Feet
+// First 400 Feet are Free
+// 2 cents per foot between 400 Feet and 2000 Feet
+// flat fare for distance over 2000 Feet and under 2500 Feet
+// Over 2500 Feet 'cannot travel that far'
+
+// 1320 feet - 400 feet (free)= 920 feet * .02 cent/foot = 18.4
+// 2640 Feet 'Cannot Travel'
+// 1584 feet- 400 feet (free)= 1,184 feet * .02cent/foot = 23.68
+
+function calculatesFarePrice(start, destination) {
 
 }
 
