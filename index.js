e6d0c44b76c02875f41df41c4cf4a009f5dddd5e
Jamond Turner <jamondturner@yahoo.com>
2022-03-26 22:39:54 -0400

    Progress

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+const hqLocation = 50
+let pickupLocation = 42
+let blocks = (hqLocation-pickupLocation)
+
+function distanceFromHqInBlocks(pickupLocation, hqLocation){
+return ((hqLocation-pickupLocation) `in ${blocks}`);
+
+
+
+}
+
+
+
+
+
+function distanceFromHqInFeet(){
+
+}
+
+
+
+
+function distanceTravelledInFeet(){
+
+}
+
+
+
+function calculatesFarePrice(start, destination){
+
+
+
+}
+
